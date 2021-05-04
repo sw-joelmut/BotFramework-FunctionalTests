8e3e1c488823281a416507b9edea2a31cd258c85
Giuliano Taliano <54330145+GiulianoDolceTalianoSantoro@users.noreply.github.com>
2021-05-04 13:50:13 -0300

    test logging in main method

--- Bots/DotNet/Consumers/CodeFirst/SimpleHostBot/Program.cs
+++ Bots/DotNet/Consumers/CodeFirst/SimpleHostBot/Program.cs
@@ -2,7 +2,11 @@
 // Licensed under the MIT License.
 
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.ApplicationInsights;
 
 namespace Microsoft.BotFrameworkFunctionalTests.SimpleHostBot
 {
@@ -14,19 +18,36 @@ namespace Microsoft.BotFrameworkFunctionalTests.SimpleHostBot
         /// <param name="args">The command line args.</param>
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false)
+                .Build();
+
+            var host = CreateHostBuilder(args, configuration).Build();
+
+            var logger = host.Services.GetRequiredService<ILogger<Program>>();
+            logger.LogError("AZURE ERROR TEST PROGRAM");
+
+            host.Run();
         }
 
         /// <summary>
         /// Creates a new instance of the <see cref="HostBuilder"/> class with pre-configured defaults.
         /// </summary>
         /// <param name="args">The command line args.</param>
+        /// <param name="configuration">The configuration properties.</param>
         /// <returns>The initialized <see cref="IHostBuilder"/>.</returns>
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        public static IHostBuilder CreateHostBuilder(string[] args, IConfiguration configuration) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>()
+                    .ConfigureLogging(
+                        builder =>
+                        {
+                            builder.AddApplicationInsights(configuration["APPINSIGHTS_INSTRUMENTATIONKEY"]);
+                            builder.AddFilter<ApplicationInsightsLoggerProvider>(string.Empty, LogLevel.Trace);
+                            builder.AddFilter<ApplicationInsightsLoggerProvider>(typeof(Program).FullName, LogLevel.Trace);
+                        });
                 });
     }
 }
--- Bots/DotNet/Consumers/CodeFirst/SimpleHostBot/Startup.cs
+++ Bots/DotNet/Consumers/CodeFirst/SimpleHostBot/Startup.cs
@@ -16,8 +16,6 @@ using Microsoft.BotFrameworkFunctionalTests.SimpleHostBot.Dialogs;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.ApplicationInsights;
 
 namespace Microsoft.BotFrameworkFunctionalTests.SimpleHostBot
 {
@@ -74,16 +72,6 @@ namespace Microsoft.BotFrameworkFunctionalTests.SimpleHostBot
                 // Register a ConfigurationChannelProvider -- this is only for Azure Gov.
                 services.AddSingleton<IChannelProvider, ConfigurationChannelProvider>();
             }
-
-            services.AddLogging(options =>
-            {
-                options.AddConsole();
-                options.SetMinimumLevel(LogLevel.Trace);
-
-                options.AddFilter<ApplicationInsightsLoggerProvider>(string.Empty, LogLevel.Trace);
-
-                options.AddApplicationInsights(Configuration["APPINSIGHTS_INSTRUMENTATIONKEY"]);
-            });
         }
 
         /// <summary>
